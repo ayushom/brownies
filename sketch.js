f9023e8dc242f11defca65d6f1265b749f48a8fb
ayushom <75958119+ayushom@users.noreply.github.com>
2021-03-20 21:05:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+Ingredient
+½ cup butter
+1 cup white sugar
+2 eggs
+1 teaspoon vanilla extract
+⅓ cup unsweetened cocoa powder
+½ cup all purpose flour
+¼ teaspoon salt
+¼ teaspoon baking powder
+
+Recipe
+Step 1: Preheat oven to 350°. Grease and flour a pan.
+Step 2: In a medium bowl add some melted butter and  stir in the sugar, eggs and vanilla extract. Then beat in the coco powder, flour, salt and baking powder then mix. When done pour the batter into a pan and put in oven.
+ Step 3: Bake in preheated oven for 25-30 minutes.
+Step 4 Once done let the brownie cool down for 5-10 minutes.
